aa8bbf2bf9212cb33684482ca54071fd69a27481
Eli <Elibo@Elis-iMac.local>
2016-06-11 22:32:31 +0200

    first commit

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour
+{
+    public float vel = 100;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(vel * Time.deltaTime, 0, 0);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") {
+            DestroyObject(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/agent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class agent : MonoBehaviour {
+
+
+    public Transform target;
+    private NavMeshAgent agnte;
+	// Use this for initialization
+	void Start () {
+        agnte = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agnte.SetDestination(target.position);
+	}
+}
